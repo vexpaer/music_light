c730a2534b042915cfed8b3d1c8ba0167d2ed1fa
vexpaer <2161884681@qq.com>
2022-09-11 21:08:06 +0800

    update

---
+++ songList.js
@@ -0,0 +1,36 @@
+/**
+ * Created by smohan on 2016/11/19.
+ * Edit by vexpaer on 2022/9/11
+ */
+var songList = [
+    { title:'24kGoldn、iann dior - Mood.mp3',singer:'NULL',audio:'music/24kGoldn、iann dior - Mood.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Aaron Smith、Luvli - Dancin (Krono Remix).mp3',singer:'NULL',audio:'music/Aaron Smith、Luvli - Dancin (Krono Remix).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'AJR - Bang!.mp3',singer:'NULL',audio:'music/AJR - Bang!.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'AJR - Burn the House Down.mp3',singer:'NULL',audio:'music/AJR - Burn the House Down.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'AJR - I Won’t (Explicit).mp3',singer:'NULL',audio:'music/AJR - I Won’t (Explicit).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Classical Artists - 春之声圆舞曲.mp3',singer:'NULL',audio:'music/Classical Artists - 春之声圆舞曲.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Daisy the Great、AJR - Record Player.mp3',singer:'NULL',audio:'music/Daisy the Great、AJR - Record Player.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Doja Cat - Say So (Explicit).mp3',singer:'NULL',audio:'music/Doja Cat - Say So (Explicit).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Hip Hop Ninja Beats - Retro Funk.mp3',singer:'NULL',audio:'music/Hip Hop Ninja Beats - Retro Funk.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Imagine Dragons - Bones.mp3',singer:'NULL',audio:'music/Imagine Dragons - Bones.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Imagine Dragons、Broiler - Shots (Broiler Remix).mp3',singer:'NULL',audio:'music/Imagine Dragons、Broiler - Shots (Broiler Remix).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Katie Sky - Monsters.mp3',singer:'NULL',audio:'music/Katie Sky - Monsters.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Lady Gaga - Poker Face.mp3',singer:'NULL',audio:'music/Lady Gaga - Poker Face.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Lazer Boomerang - Time to Pretend.mp3',singer:'NULL',audio:'music/Lazer Boomerang - Time to Pretend.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Lindsey Stirling - Masquerade.mp3',singer:'NULL',audio:'music/Lindsey Stirling - Masquerade.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Lindsey Stirling - Underground.mp3',singer:'NULL',audio:'music/Lindsey Stirling - Underground.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Lopu$ - So Cute~.mp3',singer:'NULL',audio:'music/Lopu$ - So Cute~.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Martin Garrix、David Guetta、Jamie Scott、Romy Dya -.mp3',singer:'NULL',audio:'music/Martin Garrix、David Guetta、Jamie Scott、Romy Dya -.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Max Elto - Shadow Of The Sun.mp3',singer:'NULL',audio:'music/Max Elto - Shadow Of The Sun.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Oliver Tree - Life Goes On.mp3',singer:'NULL',audio:'music/Oliver Tree - Life Goes On.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Sheet Music Boss - Rush E (Impossible Version).mp3',singer:'NULL',audio:'music/Sheet Music Boss - Rush E (Impossible Version).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'SunsetRay - Chronos Collapse - La Campanella.mp3',singer:'NULL',audio:'music/SunsetRay - Chronos Collapse - La Campanella.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'TheFatRat、Riell - Hiding In The Blue.mp3',singer:'NULL',audio:'music/TheFatRat、Riell - Hiding In The Blue.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'Virtual Riot - Energy Drink.mp3',singer:'NULL',audio:'music/Virtual Riot - Energy Drink.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'xxxCr3 - See Tinh.mp3',singer:'NULL',audio:'music/xxxCr3 - See Tinh.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'周杰伦 - 最伟大的作品.mp3',singer:'NULL',audio:'music/周杰伦 - 最伟大的作品.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'接个吻，开一枪、HOPE T - 锦里 (纯音乐).mp3',singer:'NULL',audio:'music/接个吻，开一枪、HOPE T - 锦里 (纯音乐).mp3',thumbnail:'data/vexpaer.png' },
+    { title:'纯音乐 - 杜鹃圆舞曲.mp3',singer:'NULL',audio:'music/纯音乐 - 杜鹃圆舞曲.mp3',thumbnail:'data/vexpaer.png' },
+    { title:'许嵩 - 雅俗共赏.mp3',singer:'NULL',audio:'music/许嵩 - 雅俗共赏.mp3',thumbnail:'data/vexpaer.png' },
+      
+];
